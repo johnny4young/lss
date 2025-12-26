99d30bfc69b59b8925c75b9cb0f6e2fa9e9857ab
johnny4young <asesordeprogramacion@gmail.com>
2025-12-26 16:14:04 -0500

    chore: added types

---
+++ lss.go
@@ -0,0 +1,28 @@
+package main
+
+// file types
+const (
+	fileRegular int = iota
+	fileDirectory
+	fileExecutable
+	fileCompress
+	fileImage
+	fileText
+)
+
+// file extension
+const (
+	exe  = ".exe"
+	deb  = ".deb"
+	zip  = ".zip"
+	tar  = ".tar"
+	rar  = ".rar"
+	png  = ".png"
+	jpg  = ".jpg"
+	jpeg = ".jpeg"
+	gif  = ".gif"
+	txt  = ".txt"
+	md   = ".md"
+	csv  = ".csv"
+	json = ".json"
+)
